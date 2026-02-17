6bad7d3212a1bbf54ea70bb8122bffa666534671
klotyesz <ifj.sarkadi.g@gmail.com>
2026-02-17 13:16:11 +0100

    added sef class

--- Sef.cs
+++ Sef.cs
@@ -8,5 +8,26 @@ namespace PizzaKemence
 {
     internal class Sef
     {
+        private Kemence kemence;
+
+        private static Random r = new Random();
+        private static string[] nevek = { "Magyaros", "Sajtos", "Texsas", "Sonkás", "Gyrosos" };
+        public Sef(Kemence kemence)
+        {
+            this.kemence = kemence;
+        }
+        public void Futtat()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+                string nev = nevek[r.Next(0, nevek.Length)];
+                int meret = r.Next(16, 48);
+                Pizza feladat = new Pizza(nev, meret);
+
+                kemence.Hozzaad(feladat);
+                Console.WriteLine($"A séf leadta az alábbi piizát: {feladat}");
+            }
+        }
     }
 }
