49e761832a98cf8123b7955e1cb4fafa88bbedda
klotyesz <ifj.sarkadi.g@gmail.com>
2026-02-17 13:13:29 +0100

    Added sync puffer class

--- SzinkronPuffer.cs
+++ SzinkronPuffer.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace PizzaKemence
 {
-    internal class SzinkronPuffer
+    internal class SzinkronPuffer<T>
     {
+        private Queue<T> sor;
+        private int meret;
+        private readonly object zar = new object();
+        public SzinkronPuffer(int meret)
+        {
+            this.meret = meret;
+            this.sor = new Queue<T>();
+        }
+
+        public void Hozzaad(T elem)
+        {
+            lock (zar)
+            {
+                while (sor.Count == meret)
+                {
+                    Monitor.Wait(zar);
+                }
+                sor.Enqueue(elem);
+                Monitor.PulseAll(zar);
+
+            }
+        }
+
+        public T Kivesz()
+        {
+            lock (zar)
+            {
+                while (sor.Count == 0)
+                {
+                    Monitor.Wait(zar);
+                }
+                T elem = sor.Dequeue();
+                Monitor.PulseAll(zar);
+                return elem;
+
+            }
+        }
     }
 }
