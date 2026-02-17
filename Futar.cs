6c2ac0e00cdd41395cb6eb205554dbc06b64e609
klotyesz <ifj.sarkadi.g@gmail.com>
2026-02-17 13:24:09 +0100

    finished project

--- Futar.cs
+++ Futar.cs
@@ -8,5 +8,20 @@ namespace PizzaKemence
 {
     internal class Futar
     {
+        private Kemence kemence;
+        public Futar(Kemence nyomtato)
+        {
+            this.kemence = nyomtato;
+        }
+
+        public void Futtat()
+        {
+            while (true)
+            {
+                Thread.Sleep(3000);
+                Pizza feladat = kemence.Kivesz();
+                Console.WriteLine($"Kiszállítva: {feladat}");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Kemence kemence = new Kemence(5);
+
+            Sef sef = new Sef(kemence);
+            Futar futar = new Futar(kemence);
+
+            Thread sefSzal = new Thread(sef.Futtat);
+            Thread futarSzal = new Thread(futar.Futtat);
+
+            sefSzal.Start();
+            futarSzal.Start();
         }
     }
 }
