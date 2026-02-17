a0a83f92eb4a644b3eba57c137d317d4d03beb08
klotyesz <ifj.sarkadi.g@gmail.com>
2026-02-17 12:56:37 +0100

    Add project files.

---
+++ Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaKemence
+{
+    internal class Pizza
+    {
+        private string nev;
+        private int meret;
+
+        public Pizza(string nev, int meret)
+        {
+            this.nev = nev;
+            this.meret = meret;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Meret { get => meret; set => meret = value; }
+
+
+        public override string ToString()
+        {
+            return $"{this.nev} ({this.meret} cm)";
+        }
+    }
+}
